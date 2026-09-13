a04e78721c136628a2783bdb1818e65901c70d09
UMOPhil <philip.ramos@maine.edu>
2026-09-12 20:00:00 -0400

    Hwk 1 practice

---
+++ Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    public static void main(String[] args){
+        String title;
+        String length;
+    }
+
+    // parameterized constructor
+
+    Book(title, length){
+        this.title = title;
+        this.length = length;
+    }
+  
