1dc71f2336e1d9cebf2aa60f656d49119dd1ed3b
UMOPhil <philip.ramos@maine.edu>
2026-09-18 13:28:53 -0400

    Book and Booktester Homework #1

--- Book.java
+++ Book.java
@@ -1,13 +1,58 @@
 public class Book {
-    public static void main(String[] args){
-        String title;
-        String length;
-    }
+    
+private String title;
+private String pages;
 
-    // parameterized constructor
-
-    Book(title, length){
+    public Book(String title, String pages){
         this.title = title;
-        this.length = length;
+        this.pages = pages;
+
     }
-  
+
+    public String toString() {
+
+        return title + pages;
+    }
+
+
+}
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+// questions
+// 
+//
+//whats a constructor?
+//why do we need to use it?
+//does it need to be called from a different class?
+
+//every java file we create is a class?
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
